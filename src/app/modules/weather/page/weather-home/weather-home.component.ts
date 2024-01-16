d6ed92dd7ead2b86ecdd3c507393801d41d73058
isabellagouveias <Isabellagouveias@gmail.com>
2024-01-16 15:55:41 -0300

    implementando formulário de pesquisa

--- src/app/modules/weather/page/weather-home/weather-home.component.ts
+++ src/app/modules/weather/page/weather-home/weather-home.component.ts
@@ -1,15 +1,19 @@
 import { WeatherDatas } from 'src/app/models/interfaces/WeatherDatas';
 import { WeatherService } from './../../services/weather.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: [],
 })
-export class WeatherHomeComponent implements OnInit {
-  initialCityName = 'São Paulo'
+export class WeatherHomeComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new Subject();
+  initialCityName = 'São Paulo';
   weatherDatas!: WeatherDatas;
+  searchIcon = faMagnifyingGlass;
 
   constructor(private weatherService: WeatherService) {}
 
@@ -18,15 +22,27 @@ export class WeatherHomeComponent implements OnInit {
   }
 
   getWeatherDatas(cityName: string): void {
-    this.weatherService.getWeatherDatas(cityName).subscribe({
-      next: (response) => {
-        response && (this.weatherDatas = response);
-      },
-      error: (error) => {
-        console.log(error);
-      },
-    });
+    this.weatherService
+      .getWeatherDatas(cityName)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          response && (this.weatherDatas = response);
+        },
+        error: (error) => {
+          console.log(error);
+        },
+      });
   }
 
+  onSubmit(): void {
+    this.getWeatherDatas(this.initialCityName);
+    console.log(this.initialCityName);
+    this.initialCityName = '';
+  }
 
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
